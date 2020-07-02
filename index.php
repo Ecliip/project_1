530cb369fd39d693e75999cd5e9b8405e2d85075
Mika <prodomitor@gmail.com>
2020-07-02 18:49:14 +0200

    index.php, connected to database

--- index.php
+++ index.php
@@ -1,3 +1,45 @@
+<?php
+if(isset($_POST['submit'])) {
+
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $user = 'root';
+    $password = 'root';
+    $db = 'mydb1';
+    $host = 'localhost';
+    $port = 3307;
+
+    $link = mysqli_init();
+    $connection = mysqli_connect(
+
+        $host,
+        $user,
+        $password,
+        $db,
+        $port
+    );
+
+    if($connection){
+        echo "we are connected <br>";
+    } else {
+        die("DB connection failed!");
+    }
+}
+
+
+
+
+
+//    if($username && $password){
+//        echo "it is ok <br>";
+//    }
+//} else {
+//    echo "i didnt get the data";
+//}
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,6 +47,20 @@
     <title>Title</title>
 </head>
 <body>
+<form method="post" action=<?php $_SERVER['PHP_SELF'] ?>>
+    <div>
+        name: <input type="text" name="username">
+    </div>
+    <div>
+        password: <input type="password" name="password">
+    </div>
+    <div>
+        <input type="submit" name="submit">
+    </div>
+    <?php
 
+    ?>
+
+</form>
 </body>
 </html>
