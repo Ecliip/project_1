f8f31abcecf9ed6c034dbed2f1484aa4a8a04866
Mika <prodomitor@gmail.com>
2020-07-04 22:07:01 +0200

    created DBConnection.php file, where i connect and query MySQL

---
+++ DBConnection.php
@@ -0,0 +1,32 @@
+<?php
+    $user = 'root';
+    $password = 'root';
+    $db = 'mydb1';
+    $host = 'localhost';
+    $port = 3307;
+
+    $con ;
+     $con = mysqli_connect(
+        $host,
+        $user,
+        $password,
+        $db,
+        $port);
+
+// Check connection
+    if (mysqli_connect_errno()) {
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+        exit();
+    } else {
+        echo "it is connected <br>";
+    }
+
+global $con;
+$result = mysqli_query(
+    $con,
+    "SELECT * FROM users");
+if ($result) {
+    echo "Returned rows are: " . mysqli_num_rows($result) . "<br>";
+}
+
+
